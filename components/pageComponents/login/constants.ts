9aa5951e9688ed237909c811087ae4db251cd600
Lekan Dar <Lekandar11@gmail.com>
2022-07-28 06:53:29 -1200

    PageHead Setup

---
+++ components/pageComponents/login/constants.ts
@@ -0,0 +1,9 @@
+const PAGE_HEAD = {
+  title: "Login",
+};
+
+const constants = {
+  PAGE_HEAD,
+};
+
+export default constants;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,14 @@ import type { NextPage } from "next";
 import AdminAuthLayout from "../components/layouts/AdminAuthLayout";
 import PageHead from "../components/common/components/PageHead";
 import LoginSection from "../components/pageComponents/login/LoginSection";
+import constants from "../components/pageComponents/login/constants";
+
+const { PAGE_HEAD } = constants;
 
 const Home: NextPage = () => {
   return (
     <>
-      <PageHead title="login" />
+      <PageHead title={PAGE_HEAD.title} />
       <AdminAuthLayout>
         <LoginSection />
       </AdminAuthLayout>
